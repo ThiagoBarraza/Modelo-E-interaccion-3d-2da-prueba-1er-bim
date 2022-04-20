1c74cdfe2f9e1823e6f8fc736849cc3fef5276c3
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-04-20 15:12:26 -0300

    Initial commit

---
+++ Assets/Helado.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helado : MonoBehaviour
+{
+    public string CodigoHelado;
+    public int CantidadHelado;
+    //si el kilo de helado esta a 500$ eso significa que 1$ = 2g
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CodigoHelado == "CHO" || CodigoHelado == "FRU" || CodigoHelado == "DDL" && CantidadHelado <= 250 && CantidadHelado <= 3000)
+        {
+            float Precio = CantidadHelado / 2;
+        }
+        else
+        {
+            Debug.Log("Error, codigo invalido/ cantidad de helado fuera de rango (rango = 250 hasta 3000)");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Modelo-E-interaccion-3d-2da-prueba-1er-bim
+ Prueba de MEI pero por 2da vez
